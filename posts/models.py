608b6fb3341fef1f1554c8d48ec6c7af97100ccc
leverh <leverh@gmail.com>
2023-08-30 15:09:55 +0000

    Added isbn and author name to post model

--- posts/models.py
+++ posts/models.py
@@ -44,6 +44,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13)
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True)
