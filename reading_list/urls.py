695728ddc43ae2e90599702a006287e0d49a2eef
leverh <leverh@gmail.com>
2023-09-06 13:17:31 +0000

    Added authentication to reading_lists and also a new endpoint route to the app

--- reading_list/urls.py
+++ reading_list/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookListCreateView, add_book_to_reading_list, remove_book_from_reading_list
+from .views import BookListCreateView, add_book_to_reading_list, remove_book_from_reading_list, get_user_reading_list
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:book_id>/add/', add_book_to_reading_list, name='add-book-to-list'),
     path('books/<int:book_id>/remove/', remove_book_from_reading_list, name='remove-book-from-list'),
+    path('reading-list/', get_user_reading_list, name='user-reading-list'),
 ]
--- reading_list/views.py
+++ reading_list/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
 from .models import Book, UserBook
 from .serializers import BookSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
@@ -9,9 +9,26 @@ from django.shortcuts import get_object_or_404
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only authenticated users can create books
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def get_user_reading_list(request):
+    # Fetch the UserBook objects for the logged-in user
+    user_books = UserBook.objects.filter(user=request.user)
+
+    # Extract the associated Book objects
+    books = [user_book.book for user_book in user_books]
+
+    # Serialize the books
+    serialized_books = BookSerializer(books, many=True)
+
+    return Response(serialized_books.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
 def add_book_to_reading_list(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     UserBook.objects.create(user=request.user, book=book)
@@ -19,6 +36,7 @@ def add_book_to_reading_list(request, book_id):
 
 
 @api_view(['DELETE'])
+@permission_classes([permissions.IsAuthenticated])
 def remove_book_from_reading_list(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     UserBook.objects.filter(user=request.user, book=book).delete()
