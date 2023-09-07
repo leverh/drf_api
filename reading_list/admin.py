dfbdf94d51be292e78ee9bd88e9b8bd097c0a243
leverh <leverh@gmail.com>
2023-09-07 12:50:55 +0000

    Still trying to diagnose the issue- trying to change admin visibility of item

--- reading_list/admin.py
+++ reading_list/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Book
-
-# Register your models here.
+from .models import Book, UserBook
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author']
+
+# Register UserBook with the default admin interface
+
+
+admin.site.register(UserBook)
