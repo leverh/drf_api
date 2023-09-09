095c201320aa4d237c8b5aca2590c917a2f420c1
leverh <leverh@gmail.com>
2023-09-09 10:42:27 +0000

    Added Book of the Week feature

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from reviews import views
 
 urlpatterns = [
     path('reviews/', views.PostList.as_view()),
-    path('reviews/<int:pk>/', views.PostDetail.as_view())
+    path('reviews/<int:pk>/', views.PostDetail.as_view()),
+    path('book-of-the-week/', BookOfTheWeekView.as_view(), name='book-of-the-week'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -8,6 +8,8 @@ from bookhub.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your views here.
 
@@ -53,3 +55,25 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
         likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
+
+
+class BookOfTheWeekView(APIView):
+
+    def get(self, request):
+        one_week_ago = timezone.now() - timedelta(days=7)
+        recent_reviews = Review.objects.filter(created_at__gte=one_week_ago)
+        
+        top_review = max(recent_reviews, key=lambda review: review.likes_count + review.comments_count, default=None)
+        
+        if top_review:
+            response_data = {
+                'title': top_review.title,
+                'author_name': top_review.author_name,
+                'content': top_review.content,
+                'likes_count': top_review.likes_count,
+                'comments_count': top_review.comments_count,
+                'image_url': top_review.image.url if top_review.image else None,
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "No reviews found in the past week."}, status=status.HTTP_404_NOT_FOUND)
