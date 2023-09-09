ffef039ebfc4877bfbab802ffec88fcf52594fb4
leverh <leverh@gmail.com>
2023-09-09 11:04:21 +0000

    Deploying updated backend with Book of the Week feature

--- bookhub/urls.py
+++ bookhub/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
+    path('', include('reviews.urls')),
     path('', root_route),
 ]
