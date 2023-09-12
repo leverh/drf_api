572159aa863a55ffc733bccebf7919ee42ede5ac
leverh <leverh@gmail.com>
2023-09-12 12:17:14 +0000

    Changed cloudinary file name to see if it fixes the bug

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='v1694520382/Default_pfp.svg_zurque'
+        upload_to='images/', default='default_profile_qdjgyp'
     )
 
     class Meta:
